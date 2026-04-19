d9315eeb8603a443bcd15eab26c54e236d2cb3ed
Joshua Ravael <jravaellnew@gmail.com>
2026-04-19 14:17:26 +0700

    feat: add CMake configuration and test files for build installation checks

---
+++ build_install_check/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/joshuarvl/Documents/jcode
+# Build directory: /home/joshuarvl/Documents/jcode/build_install_check
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("apps/jot")
+subdirs("tests")
---
+++ build_install_check/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/joshuarvl/Documents/jcode/tests
+# Build directory: /home/joshuarvl/Documents/jcode/build_install_check/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(jot_features "/home/joshuarvl/Documents/jcode/build_install_check/tests/jot_tests")
+set_tests_properties(jot_features PROPERTIES  _BACKTRACE_TRIPLES "/home/joshuarvl/Documents/jcode/tests/CMakeLists.txt;12;add_test;/home/joshuarvl/Documents/jcode/tests/CMakeLists.txt;0;")
