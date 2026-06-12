366cf87edf376477561771e19910032095b0dac9
josrvl <josrvl@arch>
2026-06-13 01:06:36 +0700

    Commit src/features/tree_sitter_install.h

---
+++ src/features/tree_sitter_install.h
@@ -0,0 +1,20 @@
+#ifndef TREE_SITTER_INSTALL_H
+#define TREE_SITTER_INSTALL_H
+
+#include <string>
+#include <vector>
+
+struct TreeSitterInstallCommand {
+  bool supported = false;
+  std::string language;
+  std::string command;
+  std::string message;
+};
+
+namespace TreeSitterInstall {
+const std::vector<std::string> &supported_languages();
+bool is_supported_language(const std::string &language);
+TreeSitterInstallCommand command_for_language(const std::string &language);
+} // namespace TreeSitterInstall
+
+#endif
